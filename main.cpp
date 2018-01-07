4b87c3035dc6571fc61fee8a12c48535ef362e23
Teemu Sarapisto <tsarpf@gmail.com>
2018-01-07 20:57:13 +0200

    Add libnoise

--- main.cpp
+++ main.cpp
@@ -1,65 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+
+#include <cstdio>
+#include <cstdlib>
 
 #include <GL/glew.h>
 
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+#include <noise/noise.h>
+
 using namespace glm;
+using namespace noise;
 
 #define GLSL(src) #src
 
 static void error_callback(int error, const char *description) {
-  fputs(description, stderr);
+    fputs(description, stderr);
 }
 
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
 static void render(GLFWwindow *window) {
-  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_ACCUM_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_ACCUM_BUFFER_BIT);
 }
 
 int main(void) {
-  GLFWwindow *window;
+    GLFWwindow *window;
 
-  glfwSetErrorCallback(error_callback);
+    glfwSetErrorCallback(error_callback);
 
-  if (!glfwInit())
-    exit(EXIT_FAILURE);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-  window = glfwCreateWindow(640, 480, "OpenGL Boilerplate", NULL, NULL);
-  if (!window) {
+    window = glfwCreateWindow(640, 480, "OpenGL Boilerplate", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    module::Perlin myModule;
+    double value = myModule.GetValue(1.25, 0.75, 0.5);
+    std::cout << value << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        render(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        //glfwWaitEvents();
+    }
+
+    glfwDestroyWindow(window);
+
     glfwTerminate();
-    exit(EXIT_FAILURE);
-  }
-
-  glewExperimental = GL_TRUE;
-  glewInit();
-
-  glfwMakeContextCurrent(window);
-
-  glfwSetKeyCallback(window, key_callback);
-
-  while (!glfwWindowShouldClose(window)) {
-    render(window);
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-    //glfwWaitEvents();
-  }
-
-  glfwDestroyWindow(window);
-
-  glfwTerminate();
-  exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
