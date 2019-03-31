086ba73d7327fd89a0b825f3b3e2ee24b2c22b5f
Teemu Sarapisto <tsarpf@gmail.com>
2019-04-01 02:53:59 +0300

    Keep osx compatibility

--- src/utils.cpp
+++ src/utils.cpp
@@ -42,10 +42,10 @@ GLFWwindow* initialize()
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     //glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     window = glfwCreateWindow(1280, 768, "Dual contouring methods", NULL, NULL);
